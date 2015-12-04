215d2e448bbf1de5efaba8f4efceae2741abb86d
BePsvPT <freedom@csie.io>
2015-12-05 02:06:31 +0800

    Fix breadcrumb too long bug

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -1,11 +1,9 @@
 @if (count($breadcrumbs))
     <nav>
         <div class="nav-wrapper blue">
-            <div class="col s12">
-                @foreach ($breadcrumbs as $breadcrumb)
-                    <a href="{{ $breadcrumb['link'] }}" class="breadcrumb">{{ $breadcrumb['text'] }}</a>
-                @endforeach
-            </div>
+            @foreach ($breadcrumbs as $breadcrumb)
+                <a href="{{ $breadcrumb['link'] }}" class="breadcrumb">{{ $breadcrumb['text'] }}</a>
+            @endforeach
         </div>
     </nav>
 @endif
