e8ff4e213c278943c63ed0a4bf513888b0e6f433
BePsvPT <freedom@csie.io>
2016-04-25 14:17:33 +0800

    Add helpers

---
+++ app/Libraries/helpers.php
@@ -0,0 +1,27 @@
+<?php
+
+if (! function_exists('human_filesize')) {
+    /**
+     * Make file size human readable.
+     *
+     * @param int $size
+     * @param int $precision
+     * @return string
+     */
+    function human_filesize($size, $precision = 2)
+    {
+        $units = ['B','KB','MB','GB','TB','PB','EB','ZB','YB'];
+
+        $step = 1024;
+
+        $i = 0;
+
+        while (($size / $step) > 0.9) {
+            $size = $size / $step;
+
+            $i++;
+        }
+
+        return round($size, $precision).' '.$units[$i];
+    }
+}
