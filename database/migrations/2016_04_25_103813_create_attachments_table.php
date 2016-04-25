bda425da747353b24538cdf151bfcf323c208cf6
BePsvPT <freedom@csie.io>
2016-04-25 14:13:37 +0800

    Create attachments and documents tables

---
+++ database/migrations/2016_04_25_103813_create_attachments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->morphs('attachmentable');
+            $table->string('name');
+            $table->string('file_name');
+            $table->string('mime_type', 32);
+            $table->unsignedMediumInteger('size');
+            $table->unsignedMediumInteger('downloads')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('attachments');
+    }
+}
