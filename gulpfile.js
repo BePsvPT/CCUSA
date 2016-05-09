d503d35f0b03a645a112bfadf11baf2372aa7da8
BePsvPT <freedom@csie.io>
2016-05-09 18:10:53 +0800

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,14 @@
-require('dotenv').load();
-
-if ('true' === process.env.DISABLE_GULP_NOTIFY) {
-    process.env.DISABLE_NOTIFIER = true;
-}
+process.env.DISABLE_NOTIFIER = true;
 
 var elixir = require('laravel-elixir');
 
 elixir.config.publicPath = 'public/assets';
 
 elixir(function(mix) {
-    mix.sass(['app.scss'])
-       .scripts(['app.js', 'zinc-analytics.js']);
+  mix.sass(['app.scss'])
+    .scripts(['app.js', 'zinc-analytics.js']);
 
-    if ('true' === process.env.BROWSER_SYNC) {
-        mix.browserSync({
-            proxy: 'localhost:8000'
-        });
-    }
-
-    if ('true' === process.env.PHP_UNIT) {
-        mix.phpUnit();
-    }
+  mix.browserSync({
+    proxy: 'ccusa.dev'
+  });
 });
