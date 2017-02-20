e02dfa665f33831523d4af4d5c3a7ca5da8267fe
ejk2013421 <ejk2013421@gmail.com>
2017-02-20 15:52:10 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 
         <div class="card-action">
           <a href="{{ route('zinc.index') }}" class="flow-text">
-            <span>Zinc 會刊 <i class="fa fa-external-link-square"></i></span>
+            <span>CCU Zinc <i class="fa fa-external-link-square"></i></span>
           </a>
         </div>
       </div>
