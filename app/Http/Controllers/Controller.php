453e27d37545439b4e9bff91fc978eb1bf0fb0b8
BePsvPT <freedom@csie.io>
2017-05-14 21:32:44 +0800

    Init open graph and add hashid decode method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,14 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use ChrisKonnertz\OpenGraph\OpenGraph;
+use Hashids;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var OpenGraph
+     */
+    protected $og;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->initOg();
+    }
+
+    /**
+     * 初始化 Facebook Open Graph.
+     */
+    protected function initOg()
+    {
+        $this->og = new OpenGraph;
+
+        $this->og
+            ->title('國立中正大學學生會')
+            ->url(request()->url())
+            ->type('website')
+            ->locale('zh_TW');
+
+        view()->share('og', $this->og);
+    }
 
     /**
      * Return 200 http response.
@@ -21,5 +53,27 @@ class Controller extends BaseController
     public function ok(array $headers = [])
     {
         return response('', 200, $headers);
+    }
+
+    /**
+     * Decode hashid to original number.
+     *
+     * @param string $hashid
+     *
+     * @return int
+     */
+    protected function decodeHashid($hashid)
+    {
+        if (false !== strpos($hashid, '-')) {
+            $hashid = array_last(explode('-', $hashid));
+        }
+
+        $ids = Hashids::decode($hashid);
+
+        if (empty($ids)) {
+            throw new NotFoundHttpException;
+        }
+
+        return array_first($ids);
     }
 }
