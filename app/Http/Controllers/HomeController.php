8756496df196a1713a0ce5fbfbb3d905f98fb524
BePsvPT <freedom@csie.io>
2017-05-14 21:34:19 +0800

    Add open graph image and update comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 class HomeController extends Controller
 {
     /**
-     * Get the application home page.
+     * 學生會首頁.
      *
      * @return \Illuminate\View\View
      */
     public function home()
     {
+        $this->og->image(asset('assets/media/images/general/logo/ccusa.png'));
+
         return view('home');
     }
 }
