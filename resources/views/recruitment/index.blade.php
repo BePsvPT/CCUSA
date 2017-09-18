98fa9c0ff7a37f4cfe8d3f9f3d2ccf0b4feb7875
ejk2013421 <ejk2013421@gmail.com>
2017-09-19 00:50:59 +0800

    Update index.blade.php

--- resources/views/recruitment/index.blade.php
+++ resources/views/recruitment/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', '2017秋徵 | 國立中正大學學生會')
 
 @php
-header('Location:https://ccusa.ccu.edu.tw/recruitment'.rand(1,5).'.php');
+header('Location:https://ccusa.ccu.edu.tw/recruitment-'.rand(1,5).'.php');
 @endphp
 
 @section('main')
