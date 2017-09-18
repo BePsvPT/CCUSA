d1bc4223511e25aedabf913edc693df8ecbcc9bb
ejk2013421 <ejk2013421@gmail.com>
2017-09-18 17:20:14 +0800

    Update RecruitmentController.php

--- app/Http/Controllers/RecruitmentController.php
+++ app/Http/Controllers/RecruitmentController.php
@@ -8,27 +8,15 @@ class RecruitmentController extends Controller
      * 徵才.
      * @return \Illuminate\View\View
      */
-    /*public function index()
+    public function index()
     {
         
         // 設定 facebook open graph
-        $this->og->title('2017秋徵 | 國立中正大學學生會')
-            ->image(asset('assets/media/images/general/guide/cooperative-store.jpg'));
+        $this->og->title('2017秋徵 | 國立中正大學學生會')->image(asset('assets/media/images/general/guide/cooperative-store.jpg'));
         //return redirect('/')->setTargetUrl('https://goo.gl/nDQ1rB');
         return view('recruitment.index');
     }
 
 
-}*/
-
-    /**
-     * 學生會首頁.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function home()
-    {
-        $this->og->image(asset('assets/media/images/general/logo/ccusa.png'));
-        return view('home');
-    }
+}
 }
