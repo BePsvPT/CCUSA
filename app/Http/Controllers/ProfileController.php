15eac3117e91abafff8e3dfa2872afadd04b2267
ejk2013421 <ejk2013421@gmail.com>
2018-12-04 21:40:49 +0800

    test profile view

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 class ProfileController extends Controller
 {
-
+    /**
+     * 學生會簡介.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
+        $this->og->image(asset('assets/media/images/general/logo/ccusa.png'));
+
         return view('profile.index');
     }
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,15 +4,19 @@
 
 @section('main')
     <div class="row">
-        <button  id="heading_center" class="col btn text-left container-fluid collapsed" data-toggle="collapse" data-target="#center_collapse" >
-            行政中心
-        </button>
-        <div class="col">
-            <div class="collapse" id="center_collapse"  data-parent="#data_view">
-                <p>
-                    會長 - XXX :
-                        XX系......
-                </p>
+        <div class="row">
+            <button  id="heading_center" class="col btn text-left container-fluid collapsed" data-toggle="collapse" data-target="#center_collapse" >
+                行政中心
+            </button>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="collapse" id="center_collapse"  data-parent="#data_view">
+                    <p>
+                        會長 - XXX :
+                            XX系......
+                    </p>
+                </div>
             </div>
         </div>
     </div>
