069e6c9a807c0f6ed246160d84ed528d6d6eec91
ejk2013421 <ejk2013421@gmail.com>
2018-12-04 23:04:55 +0800

    modify profile view (add JS)

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,17 +3,25 @@
 @section('title', 'PROFILE | 國立中正大學學生會')
 
 @section('main')
+    <script rel="script" type="application/javascript">
+        function show(id) {
+            document.getElementById('center_collapse').hidden=true;
+
+            document.getElementById(id).hidden=false;
+        }
+    </script>
+
     <div class="row">
         <div class="row">
-            <button  id="heading_center" class="col btn text-left container-fluid collapsed" data-toggle="collapse" data-target="#center_collapse" >
+            <button  id="heading_center" class="col btn text-left container-fluid collapsed" data-toggle="collapse" data-target="#center_collapse" onclick="show('center_collapse');">
                 行政中心
             </button>
         </div>
         <div class="row">
             <div class="col">
-                <div class="collapse" id="center_collapse"  data-parent="#data_view">
+                <div class="collapse" id="center_collapse"  data-parent="#data_view" hidden>
                     <p>
-                        會長 - XXX :
+                        會長 - XXX :<br/>
                             XX系......
                     </p>
                 </div>
